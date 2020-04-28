db8d793c5846139323340ee7309a89c65df93f8e
kirankatte12 <kiran.katte12@gmail.com>
2020-04-28 23:57:09 +1000

    Template of Rail Fence Cipher

--- main.c
+++ main.c
@@ -1,63 +1,62 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-main()
-{
- int i,j,len,rails,count,code[100][1000];
-    char str[1000];
-    printf("Enter a Secret Message\n");
-    gets(str);
-    len=strlen(str);
- printf("Enter number of rails\n");
- scanf("%d",&rails);
- for(i=0;i<rails;i++)
- {
-  for(j=0;j<len;j++)
-  {
-   code[i][j]=0;
-  }
- }
-count=0;
-j=0;
-while(j<len)
-{
-if(count%2==0)
-{
- for(i=0;i<rails;i++)
- {
-  //strcpy(code[i][j],str[j]);
-  code[i][j]=(int)str[j];
-  j++;
- }
 
-}
-else
-{
 
- for(i=rails-2;i>0;i--)
- {
-  code[i][j]=(int)str[j];
- j++;
- }
+int main(){
+
+char str[1000]="wearediscoveredfleeatonce", *ptr;
+ptr = &str;
+int length = 25, rails = 3, i, j, count, matrixarray[100][1000];
+
+
+
+while(*ptr != '\0')
+{
+printf("%c", *ptr++);
 }
 
-count++;
-}
+printf("\nLength: %d\n", length);
+printf("Rails: %d\n", rails);
 
-for(i=0;i<rails;i++)
+for(i=0; i<rails; i++)
 {
- for(j=0;j<len;j++)
- {
-  if(code[i][j]!=0)
-  printf("%c",code[i][j]);
- }
-
+    for(j=0; j<length;j++)
+    {
+        matrixarray[i][j]=0;
+    }
+}
+count = 0;
+j = 0;
+while(j<length)
+{
+    if(count%2==0)
+    {
+        for(i=0; i<rails;i++)
+        {
+            matrixarray[i][j] = (int)str[j];
+            j++;
+        }
+    }
+    else
+    {
+        for(i=rails-2; i>0; i--)
+        {
+            matrixarray[i][j] = (int)str[j];
+            j++;
+        }
+    }
+count ++;
+}
+for(i=0; i<rails;i++)
+{
+    for(j=0;j<length;j++)
+    {
+        if(matrixarray[i][j] != 0)
+            printf("%c", matrixarray[i][j]);
+    }
 }
 printf("\n");
 }
-
-
-
-
 
 
